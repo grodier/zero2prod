2fb2667fb27e0a115c46be285bd62c6fa21be6ca
grodier <grodier3@gmail.com>
2022-08-13 20:41:32 -0400

    Refactor main code to library

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/healthcheck", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    zero2prod::run().await
 }
