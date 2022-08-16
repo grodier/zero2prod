4c03803eb517654955c6ff698704b51dbf5f7570
grodier <grodier3@gmail.com>
2022-08-15 20:51:38 -0400

    Refactor folder structure

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,12 @@
+use actix_web::{web, HttpResponse};
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    #[allow(dead_code)]
+    email: String,
+    #[allow(dead_code)]
+    name: String,
+}
+pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
