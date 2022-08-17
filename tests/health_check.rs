fbe59a1093c3caff1b2d7784c798826e0ddc62b1
grodier <grodier3@gmail.com>
2022-08-16 23:37:27 -0400

    Connect to database as part of health_check test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,6 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
-use zero2prod::startup;
+use zero2prod::{configuration, startup};
 
 #[tokio::test]
 async fn health_check_works() {
@@ -19,11 +20,17 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let address = spawn_app();
+    let configuration = configuration::get_configuration().expect("Failed to read configuration.");
+    let configuration_string = configuration.database.connection_string();
+    println!("CONFIGURATION STRING: {}", &configuration_string);
+    let connection = PgConnection::connect(&configuration_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
-        .post(&format!("{}/subscriptions", address))
+        .post(&format!("{}/subscriptions", &address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
