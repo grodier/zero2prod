992c0e80ce8e3c577c268cbb2734b835272ad042
grodier <grodier3@gmail.com>
2022-08-21 20:54:32 -0400

    Setup database with connection pool with boilerplate for tests to past

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration;
 use zero2prod::startup;
@@ -5,7 +6,10 @@ use zero2prod::startup;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = configuration::get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    startup::run(listener)?.await
+    startup::run(listener, connection_pool)?.await
 }
