66a709b1190f395e9ca1f4a35eb5a99ebcffbdef
grodier <grodier3@gmail.com>
2022-08-28 17:12:11 -0400

    Provide more info to logs

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,7 +11,11 @@ pub struct FormData {
     name: String,
 }
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    log::info!("Saving new subscriber details in the database");
+    log::info!(
+        "Adding '{}' '{}' as a new subscriber.",
+        form.email,
+        form.name
+    );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
