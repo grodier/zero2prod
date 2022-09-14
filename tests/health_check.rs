d17b943bcb3a7bad8fa056797cbb261008ca6779
grodier <grodier3@gmail.com>
2022-09-13 23:33:20 -0400

    Refactor to use SubscriberEmail from domain and fix test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -46,16 +46,13 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     let app = spawn_app().await;
     let client = reqwest::Client::new();
 
     let test_cases = vec![
         ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
-        (
-            "name=Ursula&email=ursula_le_guin%40gmail.com",
-            "empty email",
-        ),
+        ("name=Ursula&email=", "empty email"),
         ("name=Ursula&email=definitely_not_an_email", "invalid email"),
     ];
 
